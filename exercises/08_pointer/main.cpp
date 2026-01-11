cb4f34a9206a58e2df048c68d0903f5c0e18ab01
youngShark8 <searchykl@163.com>
2026-01-11 17:58:26 +0800

    pass exercise 8

--- exercises/08_pointer/main.cpp
+++ exercises/08_pointer/main.cpp
@@ -5,6 +5,15 @@ bool is_fibonacci(int *ptr, int len, int stride) {
     ASSERT(len >= 3, "`len` should be at least 3");
     // TODO: 编写代码判断从 ptr 开始，每 stride 个元素取 1 个元素，组成长度为 n 的数列是否满足
     // arr[i + 2] = arr[i] + arr[i + 1]
+    for (int i = 0; i + 2 < len; ++i) {
+        int arr0 = ptr[i * stride];
+        int arr1 = ptr[(i + 1) * stride];
+        int arr2 = ptr[(i + 2) * stride];
+
+        if (arr2 != arr0 + arr1) {
+            return false;
+        }
+    }
     return true;
 }
 
